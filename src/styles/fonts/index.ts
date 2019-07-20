6e4eb649525189658a24fa388e5864ca1c1b791c
calamberia <calamberia@gmail.com>
2019-07-20 22:59:57 +0200

    adds light fonts

--- src/styles/fonts/index.ts
+++ src/styles/fonts/index.ts
@@ -1,9 +1,11 @@
 import { css } from "../index";
 
-import * as gilroyBoldWoff from "./gilroy-bold-webfont.woff";
-import * as gilroyBoldWoff2 from "./gilroy-bold-webfont.woff2";
-import * as gilroyMediumWoff from "./gilroy-medium-webfont.woff";
-import * as gilroyMediumWoff2 from "./gilroy-medium-webfont.woff2";
+import * as gilroyBoldWoff from "./gilroy-bold.woff";
+import * as gilroyBoldWoff2 from "./gilroy-bold.woff2";
+import * as gilroyMediumWoff from "./gilroy-medium.woff";
+import * as gilroyMediumWoff2 from "./gilroy-medium.woff2";
+import * as gilroyLightmWoff from "./gilroy-light.woff";
+import * as gilroyLightWoff2 from "./gilroy-light.woff2";
 
 export default css`
   @font-face {
@@ -19,6 +21,14 @@ export default css`
     src: url('${gilroyBoldWoff2}') format('woff2'),
          url('${gilroyBoldWoff}') format('woff');
     font-weight: bold;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Gilroy';
+    src: url('${gilroyLightWoff2}') format('woff2'),
+         url('${gilroyLightmWoff}') format('woff');
+    font-weight: lighter;
     font-style: normal;
   }
 `;
