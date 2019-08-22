320eb21c534d42ed7a16ae3459765ca10418f87e
calamberia <calamberia@gmail.com>
2019-08-22 23:03:11 +0200

    removes empty params from query string

--- src/store/studio/effects.ts
+++ src/store/studio/effects.ts
@@ -1,15 +1,21 @@
 import apiEndpoint from "@src/services/api";
 import { studios } from "@src/Config/Constants";
+import { removeEmptyFields } from "@src/helpers";
 
 export const fetchStudiosList = async (
   nextPageToken: string,
   limit: number = studios.fetchLimit,
-) => await apiEndpoint.get("", { params: { limit, nextPageToken } });
+) => {
+  const params = removeEmptyFields({ limit, nextPageToken });
+  return await apiEndpoint.get("", { params });
+};
 
 export const fetchFilteredStudiosList = async (
   nextPageToken: string,
   city: string,
   query: string,
   limit: number = studios.fetchLimit,
-) =>
-  await apiEndpoint.get("", { params: { limit, query, city, nextPageToken } });
+) => {
+  const params = removeEmptyFields({ limit, query, city, nextPageToken });
+  return await apiEndpoint.get("", { params });
+};
