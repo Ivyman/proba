ac8cc14a6308d39b286653f4eacfe9b9d410c2ee
calamberia <calamberia@gmail.com>
2019-10-17 22:28:34 +0200

    removes 'removeEmptyFields' method from effects.ts

--- src/store/studio/effects.ts
+++ src/store/studio/effects.ts
@@ -1,17 +1,16 @@
 import apiEndpoint, { URLS } from "@src/services/api";
 import { Studios } from "@src/config/Constants";
-import { removeEmptyFields } from "@src/helpers/common";
 import { IFiltersData } from "@src/types/studio";
 
 export const fetchStudiosList = async (
   nextPageToken?: string,
   filtersData?: IFiltersData,
 ) => {
-  const params = removeEmptyFields({
+  const params = {
     limit: Studios.fetchLimit,
-    query: filtersData && filtersData.query,
-    city: filtersData && filtersData.city.join(","),
+    query: filtersData ? filtersData.query : "",
+    city: filtersData ? filtersData.city.join(",") : "",
     nextPageToken,
-  });
+  };
   return await apiEndpoint.get(URLS.studiosList, { params });
 };
