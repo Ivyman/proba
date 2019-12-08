a00b7cc77ab0465db19e0401643b07ed1e71f211
calamberia <calamberia@gmail.com>
2019-12-08 20:41:25 +0100

    Added palette colors

--- src/styles/theme/palette.ts
+++ src/styles/theme/palette.ts
@@ -1,4 +1,11 @@
 export default {
-  main: "#fbfbfb",
+  primary: "#0e3496",
+  secondary: "#1a56eb",
+  blue: "#009fff",
+  turkus: "#1dc8ea",
+  black: "#181818",
   white: "#ffffff",
+  gray: "#1ec9e9",
+  grayLight: "#eff4f7",
+  grayDark: "#515c63",
 };
--- src/types/theme.ts
+++ src/types/theme.ts
@@ -14,8 +14,15 @@ export type IHeadSize = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 export type IBreakpoints = { [key in keyof typeof EBreakpoints]: string };
 
 export interface IPalette {
-  main: string;
+  primary: string;
+  secondary: string;
+  blue: string;
+  turkus: string;
+  black: string;
   white: string;
+  gray: string;
+  grayLight: string;
+  grayDark: string;
 }
 
 export default interface ITheme {
