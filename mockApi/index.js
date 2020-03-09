f9a018e2b3720b6eaf7df7b63e63b9f1ce9e4a6c
calamberia <calamberia@gmail.com>
2020-03-09 13:58:55 +0100

    Update mockapi

--- mockApi/index.js
+++ mockApi/index.js
@@ -5,41 +5,29 @@ import cors from "cors";
 import responseStudiosData from "../src/mock/studios";
 import responseFiltersData from "../src/mock/filters";
 
-const app = express();
-const port = 8086;
+const APP = express();
+const PORT = 8086;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+APP.use(cors());
+APP.use(bodyParser.json());
+APP.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/studios", (req, res) => {
-    const studios = responseStudiosData.studios.filter(item => {
-        const itemName = item.name.toLocaleLowerCase();
-        let searchQuery = "";
+APP.get("/studios", (req, res) => {
+    const studios = responseStudiosData.filter(
+        item => item.address.city.key === req.query.city || !req.query.city,
+    );
 
-        if (req.query.searchQuery) {
-            searchQuery = req.query.searchQuery.toLocaleLowerCase();
-        }
-
-        return itemName.includes(searchQuery);
-    });
-
-    const result = {
-        nextPageToken: responseStudiosData.nextPageToken,
-        studios: studios,
-    };
-
-    return res.status(200).send(result);
+    return res.status(200).send(studios);
 });
 
-app.get("/filters", (req, res) => {
+APP.get("/filters", (req, res) => {
     return res.status(200).send(responseFiltersData);
 });
 
-app.post("/message", (req, res) => {
+APP.post("/message", (req, res) => {
     return res.status(200).end();
 });
 
-app.listen(port, () =>
-    console.log("\x1b[36m", `Mock api listening on port ${port}`, "\x1b[0m"),
+APP.listen(PORT, () =>
+    console.log("\x1b[36m", `Mock api listening on port ${PORT}`, "\x1b[0m"),
 );
